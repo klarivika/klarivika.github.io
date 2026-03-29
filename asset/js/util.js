ad394b524410c6708e672a1aee1ac53d93d5288f
initer3737 <puthutwahyuaji13@gmail.com>
2026-03-30 01:24:48 +0700

    make this website react look a like

--- asset/js/util.js
+++ asset/js/util.js
@@ -1,4 +1,14 @@
-
+/**
+ * @desc manipulate the dom 
+ * @param {string} selector 
+ * @param {function} cb 
+ */
+const $= (selector,cb) =>typeof cb === 'function' ? cb(document.querySelector(selector)) : document.querySelector(selector);
+/**
+ * @desc manipulate the dom 
+ * @param {string} selector 
+ */
+const All= (selector) =>document.querySelectorAll(selector);
 /**
  * @desc fetches the pages and returns the response insert it to dom
  * @param {string} page 
@@ -9,7 +19,7 @@ const page_fetcher = async ({page,target}) => {
     try {
         if(page_cache[page]){
             document.querySelector(target).innerHTML = page_cache[page];
-            return
+            return 
         }
         
         const simplyfing_data=`${page}/${page}.html`
@@ -74,6 +84,28 @@ const custom_element_creator=({name,html})=>{
     customElements.define(name, CustomElement);
 }
 
+/**
+ * @desc store and render databased on url query param
+ */
+const render_json_data=async()=>{
+    //home start
+        if(window.location.hash === "#home"){
+            const home_data=await fetcher_data()
+            console.log("home data ",home_data)
+             //tembak ke outlet
+            const home_outlet=document.querySelector(".outlet")
+            home_data.forEach((item)=>{
+                const div=document.createElement("div")
+                div.innerHTML=`
+                    <h2>${item.name}</h2>
+                    <p>${item.description}</p>
+                `
+                home_outlet.appendChild(div)
+            })
+        }
+        //home end
+}
+
 let curent_hash="";
 /**
  * @desc create route
@@ -89,41 +121,39 @@ const routes=({routes})=>{
             if(curent_hash == window.location.hash)return
             curent_hash=new_hash
         //cocokan dengan routes jika tidak maka lempar ke page not found
-        const routes_get_keys=Object.keys(routes)[Object.keys(routes).findIndex(key => key.startsWith("/"+window.location.hash.split("/")[0]))]
+        const routes_get_keys=Object.keys(routes)[Object.keys(routes).findIndex(key => key.startsWith("/"+window.location.hash.split("/")[0]))]//route saat ini
         let get_query_param_url_if_exist=window.location.hash
         console.log("routes get keys ",routes_get_keys)
         if(/\:\w+/g.test(routes_get_keys)){
             //routes get keys jadikan objects
-            
+            //merubah data params menjadi object dengan key nama param dan value dari url {id:1}
             routes_get_keys.match(/\:\w+/g)?.map(param => {
                 const array_routes=routes_get_keys.split("/") //array route saat ini
                 const get_index_of_key_params=array_routes[array_routes.findIndex(key => key === param)].substring(1) //:id => id
-                    const url="/"+window.location.hash.split("/")
-                    const value_param=url[array_routes.indexOf(":"+get_index_of_key_params)]
+                    const url="/"+window.location.hash//.split("/")
+                    const value_param=url.split("/")[array_routes.indexOf(":"+get_index_of_key_params)]
                         data_params[get_index_of_key_params]=value_param
-                console.log("testing bang ",{value_param,url,data_params})
-                console.log("urlnya ",{url})
+                // console.log("testing bang ",{value_param,url,data_params})
+                // console.log("urlnya ",url.split("/"))
             });
-           
-            const data_query_param=window.location.hash.split("/")[1]
-            get_query_param_url_if_exist={routes_get_keys,data_query_param}
-            // console.log("data query param ",params)
+            get_query_param_url_if_exist=routes_get_keys.substring(1)
+            // console.log("data query param ",routes_get_keys) 
         }
-        console.log("data data param "+get_query_param_url_if_exist)
-        console.log("data params ",data_params)
+        // console.log("data data param "+get_query_param_url_if_exist)
+        // console.log("data params ",data_params)
         const url=routes['/'+get_query_param_url_if_exist] ?? routes['/#404']; 
         await page_fetcher({page:url, target:".outlet"});
         // make dinamyc pointings
         pointings_images_video_elements()
-        //fetcher data
-        fetcher_data()
+        //fetcher data di route tertentu saja untuk menghemat resource karena tidak semua page membutuhkan data yang sama
+       render_json_data()
     })
     window.addEventListener('load',async()=>{
         // Trigger the initial route based on the current hash
         
         const url=window.location.hash
         
-        console.log("no bug baby ",url)
+        // console.log("no bug baby ",url)
         //kalau url saat ini kosong berarti render halaman home atau pertama  
         let initialUrl=routes[Object.entries(routes)[0][0]];
         //kalaiu url tidak kosong maka load yang sudah ada di url
@@ -136,8 +166,8 @@ const routes=({routes})=>{
        await page_fetcher({page:initialUrl, target:".outlet"});
         // make dinamyc pointings
         pointings_images_video_elements()
-        //fetcher data
-        fetcher_data()
+        //initial render data 
+        render_json_data()
     })
 }
 
@@ -162,20 +192,38 @@ const pointings_images_video_elements=()=>{
 }
 
 /**
- * @desc fetch data from json and insert it to html  
+ * @desc fetch data from json and insert it to html
+ * @param {object} datas 
  */
-const fetcher_data=async()=>{
-   const response=await fetch("./asset/data_json/data.json");
-   const data=await response.json();
-   data.map((item)=>{
-        //const manipulated_link_resource=
-        item["assets"].forEach(item2=>{
-                item2['path']=`./asset/data_klarifikasi/${item2["path"]}`;
-            //console.log("data2 ",item2)//return item2['path']='./asset/data_klarifikasi/'+item["assets"]['path']
+const fetcher_data=async(datas={})=>{
+    try{
+        //{id:1}
+       const fetching_data=async (datas={})=>{
+        const response=await fetch("./asset/data_json/data.json");
+       const data=await response.json();
+       data?.map((item)=>{
+            //const manipulated_link_resource=
+            item["assets"].forEach(item2=>{
+                    item2['path']=`./asset/data_klarifikasi/${item2["path"]}`;
+            })
+            
         })
-      // console.log("datas ",manipulated_link_resource)
-   })
-   console.log("data ",data)
+        
+        if(Object.keys(datas).length === 0){
+             //tembak ke outlet
+            return data
+        }
+            return data
+       }
+       if(Object.keys(datas).length === 0){
+           return await fetching_data()
+           
+       }
+
+    }catch(err){
+
+    }
+
 }
 
 export{
